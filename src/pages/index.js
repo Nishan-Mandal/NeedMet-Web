5778aac31819d173ecddf81341fcf1c83a038a4c
Kingshuk Dash <kingshukdash123@gmail.com>
2026-04-23 00:51:43 +0530

    fix: update import typo error in pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Home from "./Home";
 import ListingDetails from "./ListingDetails";
 import ListingsPage from "./ListingsPage";
 import AllCategory from "./AllCategory";
-import Maintenance from "./maintenance";
+import Maintenance from "./Maintenance";
 
 export {
     Home, 
